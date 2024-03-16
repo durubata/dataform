c2554b7ab66c1114bb876d63e676397f9a1fdc55
jaclight <imolewolede@gmail.com>
2024-03-16 00:08:06 -0500

    Update module exports in index.js and improve readme.md

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dform.cjs.production.min.js')
+  module.exports = require('./dataform.cjs.production.min.js')
 } else {
-  module.exports = require('./dform.cjs.development.js')
+  module.exports = require('./dataform.cjs.development.js')
 }
